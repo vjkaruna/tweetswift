b891f2fd9ee85db41cc5c8a0883b3dadb3a8acb5
vjkaruna <vjkaruna@gmail.com>
2014-09-26 18:07:49 -0700

    Route application.

--- tweetswift/ViewController.swift
+++ tweetswift/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
         TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
         TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuthToken!) -> Void in
             println("Got the request token")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)" )
+            UIApplication.sharedApplication().openURL(authURL)
             }, failure: { (error: NSError!) -> Void in
             println("Error with Oauth \(error)")
         })
